02981f1b7cdd6cebe861be1a96ddf0a80669ec9f
yadgire7 <47882001+yadgire7@users.noreply.github.com>
2023-09-10 15:29:25 -0700

    Create project_employees_1.sql

---
+++ project_employees_1.sql
@@ -0,0 +1,8 @@
+-- Problem: Project Employees 1
+-- Platform: Leetcode
+
+select p.project_id, round(sum(e.experience_years) / count(project_id), 2) as average_years
+from Project p
+left join Employee e
+on p.employee_id = e.employee_id
+group by project_id
